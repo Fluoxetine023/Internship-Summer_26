9fdd4c5a7c7126f13284864fdeae481c81c47a54
Fluoxetine 023 <vedanthvemula@gmail.com>
2026-06-18 12:34:49 +0530

    updating

--- SSH_model.py
+++ SSH_model.py
@@ -1,2 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
+
+v = 0.5
+w = 0.6
+
+k = np.linspace(-np.pi, np.pi, 1000)
+
+E = np.sqrt(v**2 + w**2 + 2*v*w*np.cos(k))
+
+plt.plot(k,E)
+plt.plot(k,-E)
+
+plt.grid()
+plt.show()
